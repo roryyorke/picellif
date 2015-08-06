79456563332d6108d61fbcf1486f581710cb827d
Rory Yorke <rory.yorke@gmail.com>
2015-08-06 22:15:04 +0200

    Messaging updated, version bump.
    picellif-dired-marked-files message includes filename when single file copied.
    
    Version bump.

--- picellif.el
+++ picellif.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Rory Yorke
 
 ;; Author: Rory Yorke <rory.yorke@gmail.com>
-;; Version: 0.2
+;; Version: 0.3
 ;; Package-Requires: 
 ;; Keywords: clipboard
 ;; URL: http://github.com/roryyorke/picellif
@@ -16,17 +16,21 @@
 
 ;; No change in this file from 0.1.1 to 0.2, but picellif.c has a bugfix.
 
+;; 0.2 to 0.3: send message to user on completion of picellif invocation
+
 ;; todo: if picellif fails, show error message
 
 (defun picellif-dired-marked-files ()
   "Send marked files (or current file, if none marked) in current
 Dired buffer to picellif."
   (interactive)
-  (if (equal 0 
-             (apply 'call-process "picellif" nil nil nil
-                    (dired-get-marked-files)))
-      (message "%d files sent to clipboard" (length (dired-get-marked-files)))
-    (message "error sending files to clipboard")))
+  (let ((files (dired-get-marked-files)))
+    (if (equal 0 
+	       (apply 'call-process "picellif" nil nil nil files))
+	(if (equal 1 (length files))
+	    (message "%s sent to clipboard" (car files))
+	  (message "%d files sent to clipboard" (length files)))
+    (message "error sending files to clipboard"))))
 
 (defun picellif-buffer-file ()
   "Send file associated with current buffer to picellif."
